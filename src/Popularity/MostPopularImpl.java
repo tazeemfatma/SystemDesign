68ced213e460fb8510ab0c890c2458b8a0f4ad8e
tazeem fatma <tazeemfatma@gmail.com>
2025-09-25 13:26:01 +0530

    updated popularity

--- src/Popularity/MostPopularImpl.java
+++ src/Popularity/MostPopularImpl.java
@@ -1,7 +1,6 @@
 package Popularity;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 import static Popularity.ContentUtil.content;
 import static Popularity.ContentUtil.isValid;
@@ -9,12 +8,20 @@ import static Popularity.ContentUtil.isValid;
 
 public class MostPopularImpl implements MostPopular{
     Map<Integer,Integer> contentMap;
-    int mostPupularContent;
+    int mostPopularContent;
     MostPopularImpl(){
         contentMap=new HashMap<>();
-        mostPupularContent=mostPopular();
+        mostPopularContent =mostPopular();
     }
 
+    /**
+     * Increases the popularity count of the specified content ID by 1.
+     * If the content ID is invalid or does not exist, prints an error message and returns.
+     * Updates the most popular content if the increased content becomes equally or more popular
+     * than the current most popular content.
+     *
+     * @param contentId the ID of the content whose popularity is to be increased
+     */
     @Override
     public void increasePopularity(int contentId) {
         if(!isValid(contentId) || !content.containsKey(contentId)){
@@ -24,11 +31,17 @@ public class MostPopularImpl implements MostPopular{
        content.put(contentId,content.getOrDefault(contentId,0)+1);
 
         //most recent updated having more popularity
-        if(content.getOrDefault(mostPupularContent,0) <= content.getOrDefault(contentId,0)){
-            mostPupularContent=contentId;
+        if(content.getOrDefault(mostPopularContent,0) <= content.getOrDefault(contentId,0)){
+            mostPopularContent =contentId;
         }
     }
 
+    /**
+     * Returns the most popular content ID based on its popularity count.
+     * If there is a tie for the highest popularity, the most recently updated content ID is returned.
+     *
+     * @return the content ID with the highest popularity, or the most recently updated one in case of a tie
+     */
     @Override
     public int mostPopular() {
         int popular = content.entrySet().stream()
@@ -37,14 +50,22 @@ public class MostPopularImpl implements MostPopular{
                 .limit(1)
                 .findFirst().orElse(0);
 
-        //most recent updated having more popularity
-        if(popular!=mostPupularContent &&
-                content.getOrDefault(popular,0)==content.getOrDefault(mostPupularContent,0) ){
-            return mostPupularContent;
+
+        if(popular!= mostPopularContent &&
+                content.getOrDefault(popular,0)==content.getOrDefault(mostPopularContent,0) ){
+            return mostPopularContent;
         }
     return popular;
     }
 
+    /**
+     * Decreases the popularity count of the specified content ID by 1.
+     * If the content ID is invalid or does not exist, prints an error message and returns.
+     * If the popularity becomes less than 1, it is set to 0.
+     * If the decreased content was the most popular, recalculates the most popular content.
+     *
+     * @param contentId the ID of the content whose popularity is to be decreased
+     */
     @Override
     public void decreasePopularity(int contentId) {
         if(!isValid(contentId) || !content.containsKey(contentId)){
@@ -57,11 +78,8 @@ public class MostPopularImpl implements MostPopular{
             content.put(contentId,0);
 
         //most recent updated having more popularity
-        int newMostPopular = mostPopular();
-
-        if(mostPupularContent==contentId &&
-                content.getOrDefault(mostPupularContent,0) < content.getOrDefault(newMostPopular,0)){
-            mostPupularContent=newMostPopular;
+        if(contentId== mostPopularContent){
+            mostPopularContent =mostPopular();
         }
 
     }
